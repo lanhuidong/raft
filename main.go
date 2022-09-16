652d619d38ab4e463a25aeb424a45df12bb42af2
lanhuidong <472897576@qq.com>
2022-09-16 22:31:58 +0800

    feat: 读取配置文件

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lanhuidong/raft/raft"
+)
 
 func main() {
 	fmt.Println("Hello raft!")
+	config := raft.ReadConfiguration()
+	fmt.Printf("config: %v\n", config)
 }
---
+++ raft/cluster.go
@@ -0,0 +1,47 @@
+package raft
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Cluster struct {
+	Id    string
+	Nodes map[string]Node
+}
+
+type Node struct {
+	Id      string `json:"id"`
+	Address string `json:"address"`
+	Port    uint32 `json:"port"`
+}
+
+type Configuration struct {
+	Id    string `json:"id"`
+	Nodes []Node `json:"nodes"`
+}
+
+func ReadConfiguration() Configuration {
+	configFile, err := os.Open("conf/cluster.json")
+	if err != nil {
+		fmt.Printf("open config file error: %v", err)
+		os.Exit(0)
+	}
+	defer configFile.Close()
+
+	fileContent, err := io.ReadAll(configFile)
+	if err != nil {
+		fmt.Printf("read config file error: %v", err)
+		os.Exit(0)
+	}
+
+	var config Configuration
+	err = json.Unmarshal(fileContent, &config)
+	if err != nil {
+		fmt.Printf("config file invalid: %v", err)
+		os.Exit(0)
+	}
+	return config
+}
